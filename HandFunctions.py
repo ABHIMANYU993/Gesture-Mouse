21be31e3147ee983d153625c46790276316fd114
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-06-04 14:51:03 +0530

    Implement distance calculation and closed-fist check methods

--- HandFunctions.py
+++ HandFunctions.py
@@ -63,3 +63,29 @@ class handDetector():
         else:
             fingers = [0, 0, 0, 0, 0]
         return fingers
+
+    def findDistance(self, p1, p2, img, draw=True, r=10, t=2):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+        return length, img, [x1, y1, x2, y2, cx, cy]
+
+    def allFingersClosed(self):
+        if len(self.lmList) != 0:
+            closedFingers = []
+            for i in range(5):
+                if self.tipIds[i] - 2 >= 0 and self.tipIds[i] < len(self.lmList):
+                    if self.lmList[self.tipIds[i]][2] < self.lmList[self.tipIds[i] - 2][2]:
+                        closedFingers.append(1)
+                    else:
+                        closedFingers.append(0)
+                else:
+                    closedFingers.append(0)
+            return all(finger == 1 for finger in closedFingers)
+        return False
